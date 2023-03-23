677619640bdd30dad4692bea1e1620a864a574f1
Jinwoo Kim <jinwookim@kw.ac.kr>
2023-03-23 11:19:55 +0900

    update

--- task1/echo_server.py
+++ task1/echo_server.py
@@ -5,7 +5,7 @@ PORT = 9999
 BUF_SIZE = 4096
 
 #TODO: make a server-socket
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # To make the port nuber reusable
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # To make the port number reusable
 #TODO: bind the IP, port number to the server-socket
 server.bind((IP, PORT))
 #TODO: make the socket a listening state
@@ -18,7 +18,6 @@ try:
       response = client.recv(BUF_SIZE)
       if not response: break
       #TODO: send the response back to the client
-
       print(buf.decode())
 except:
     client.close()
