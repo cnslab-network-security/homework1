a659406bfb7b7def0cbda97338973cb2a6f4005f
Jinwoo Kim <jinwookim@kw.ac.kr>
2023-03-26 00:15:39 +0900

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -5,8 +5,15 @@ output=`docker ps -aq --filter name=$name`
 
 if [ -z "$output" ]
 then
-  docker run --name $name -itd --network cs3864_net -v $(pwd):/homework1/ cs3864
+  if [[ "$OSTYPE" == "msys" ]]; then
+    docker run --name $name -itd --network cs3864_net -v /$(pwd):/homework1/ cs3864
+  else
+    docker run --name $name -itd --network cs3864_net -v $(pwd):/homework1/ cs3864
+  fi
 fi
 
-docker exec -it $name /bin/bash
-
+if [[ "$OSTYPE" == "msys" ]]; then
+  winpty docker exec -it $name bash
+else
+  docker exec -it $name /bin/bash
+fi
